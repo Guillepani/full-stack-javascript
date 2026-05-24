37502a4e05226ee70968522f0a723896165833ac
Guille <guillempaniagua@gmail.com>
2026-05-24 16:24:19 +0000

    Implementa login frontend y persistencia JWT

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,36 @@
+import { loginUser } from '../services/api'
+
+export const LoginForm = () => {
+  return `
+    <form id="login-form">
+      <input type="email" name="email" placeholder="Email" required>
+
+      <input type="password" name="password" placeholder="Contraseña" required>
+
+      <button>Login</button>
+    </form>
+  `
+}
+
+export const loginFormListeners = () => {
+  const form = document.querySelector('#login-form')
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(form)
+
+    const userData = {
+      email: formData.get('email'),
+      password: formData.get('password')
+    }
+
+    const response = await loginUser(userData)
+
+    if (response.token) {
+      localStorage.setItem('token', response.token)
+    }
+
+    console.log(response)
+  })
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,21 @@ import './styles/global.css'
 
 import { RegisterForm, registerFormListeners } from './components/RegisterForm'
 
+import { LoginForm, loginFormListeners } from './components/LoginForm'
+
 document.querySelector('#app').innerHTML = `
   <h1>MeetMoto</h1>
 
-  ${RegisterForm()}
+  <section>
+    <h2>Registro</h2>
+    ${RegisterForm()}
+  </section>
+
+  <section>
+    <h2>Login</h2>
+    ${LoginForm()}
+  </section>
 `
 
 registerFormListeners()
+loginFormListeners()
